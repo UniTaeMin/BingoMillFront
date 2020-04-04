2e3a802d3ab42497446d8337aa90e8c9f391c846
UniTaeMin <onnu0802@naver.com>
2020-04-05 04:13:11 +0900

    Update Home Page
    Update Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,72 +1,75 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app clipped width="300">
-      <!-- :floating="true" -->
-      <v-list dense>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-view-dashboard</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Dashboard</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-settings</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Settings</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar app clipped-left color="white" :flat="true">
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" color="#6c63ff" />
-      <v-toolbar-title class="edit__title pa-1">빙고 제작소</v-toolbar-title>
-    </v-app-bar>
-    <v-content>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center"></v-row>
-      </v-container>
-    </v-content>
-  </v-app>
+  <div class="home">
+    <img src="../assets/bingoLanding.svg" alt="빙고 사진" class="landingImage" />
+    <div class="home__title">빙고 제작소</div>
+    <div class="home__start" @click="start">빙고 만들기</div>
+    <div class="home__museum">빙고 박물관</div>
+  </div>
 </template>
 
 <script>
 export default {
-  props: {
-    source: String
-  },
-  data() {
-    return {
-      drawer: null,
-      items: [
-        { icon: "mdi-trending-up", text: "Most Popular" },
-        { icon: "mdi-youtube-subscription", text: "Subscriptions" },
-        { icon: "mdi-history", text: "History" },
-        { icon: "mdi-playlist-play", text: "Playlists" },
-        { icon: "mdi-clock", text: "Watch Later" }
-      ],
-      items2: [
-        { picture: 28, text: "Joseph" },
-        { picture: 38, text: "Apple" },
-        { picture: 48, text: "Xbox Ahoy" },
-        { picture: 58, text: "Nokia" },
-        { picture: 78, text: "MKBHD" }
-      ]
-    };
-  },
-  created() {}
+  name: "Home",
+  components: {},
+  methods: {
+    start() {
+      this.$router.push("/edit");
+    }
+  }
 };
 </script>
-
 <style scoped>
-.v-app-bar {
-  border-bottom: 1px solid rgba(0, 0, 0, 0.12) !important;
+.home {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
-.edit__title {
+.home__title {
   font-family: "NanumSEB";
+  font-size: 40px;
+  margin-top: 20px;
+  margin-bottom: 12px;
+}
+.landingImage {
+  width: 300px;
+  pointer-events: none;
+}
+.home__start {
+  width: 200px;
+  height: 48px;
   color: #6c63ff;
+  border-radius: 30px;
+  margin-top: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 3px;
+  cursor: pointer;
+  font-size: 18px;
+  font-weight: 600;
+  border: 2px solid #6c63ff;
+}
+.home__museum {
+  width: 200px;
+  height: 48px;
+  background-color: #6c63ff;
+  border-radius: 30px;
+  margin-top: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 3px;
+  color: white;
+  cursor: pointer;
+  font-weight: 600;
+  font-size: 18px;
+}
+@media screen and (max-width: 768px) {
+  .landingImage {
+    width: 80%;
+  }
 }
 </style>
