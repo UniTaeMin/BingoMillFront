1ca2aec705d48e495f269dfdfd08815acac6437c
UniTaeMin <onnu0802@naver.com>
2020-04-04 23:31:50 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,51 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Roboto&display=swap");
+@font-face {
+  font-family: "NanumSR";
+  src: url("./assets/font/NanumSquareR.ttf") format("truetype");
+  font-display: swap;
+}
+@font-face {
+  font-family: "NanumSB";
+  src: url("./assets/font/NanumSquareB.ttf") format("truetype");
+  font-display: swap;
+}
+@font-face {
+  font-family: "NanumSRB";
+  src: url("./assets/font/NanumSquareRoundB.ttf") format("truetype");
+  font-display: swap;
+}
+@font-face {
+  font-family: "ProductSansM";
+  src: url("./assets/font/ProductSans-Black.ttf") format("truetype");
+  font-display: swap;
+}
+@font-face {
+  font-family: "ProductSansR";
+  src: url("./assets/font/ProductSans-Medium.ttf") format("truetype");
+  font-display: swap;
+}
+@font-face {
+  font-family: "NanumSEB";
+  src: url("./assets/font/NanumSquareEB.ttf") format("truetype");
+  font-display: swap;
+}
+* {
+  box-sizing: border-box;
+  font-family: "Roboto", sans-serif;
+}
+body {
+  margin: 0px;
+  padding: 0px;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  width: 100%;
+  height: 100vh;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,72 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app clipped width="300">
+      <!-- :floating="true" -->
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app clipped-left color="white" :flat="true">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" color="#6c63ff" />
+      <v-toolbar-title class="edit__title pa-1">빙고 제작소</v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center"></v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  props: {
+    source: String
+  },
+  data() {
+    return {
+      drawer: null,
+      items: [
+        { icon: "mdi-trending-up", text: "Most Popular" },
+        { icon: "mdi-youtube-subscription", text: "Subscriptions" },
+        { icon: "mdi-history", text: "History" },
+        { icon: "mdi-playlist-play", text: "Playlists" },
+        { icon: "mdi-clock", text: "Watch Later" }
+      ],
+      items2: [
+        { picture: 28, text: "Joseph" },
+        { picture: 38, text: "Apple" },
+        { picture: 48, text: "Xbox Ahoy" },
+        { picture: 58, text: "Nokia" },
+        { picture: 78, text: "MKBHD" }
+      ]
+    };
+  },
+  created() {}
+};
 </script>
+
+<style scoped>
+.v-app-bar {
+  border-bottom: 1px solid rgba(0, 0, 0, 0.12) !important;
+}
+.edit__title {
+  font-family: "NanumSEB";
+  color: #6c63ff;
+}
+</style>
