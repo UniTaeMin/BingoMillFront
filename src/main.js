5f9e30411c2d1c477d6066b1716a26a402a24b67
UniTaeMin <onnu0802@naver.com>
2020-04-11 22:25:00 +0900

    Add VueFire
    Add VueFire

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import Sortable from 'vue-sortable'
 import VueHtml2Canvas from 'vue-html2canvas';
+import { firestorePlugin } from 'vuefire'
 import 'babel-polyfill'; //add this line
 
+Vue.use(firestorePlugin)
 Vue.use(Sortable)
 Vue.use(VueHtml2Canvas);
+
 Vue.config.productionTip = false
 
 new Vue({
